9cb6e751aea44e09ea1f5e218369d21a4877bd2e
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-06-04 17:14:03 +0300

    change questions

--- js/script.js
+++ js/script.js
@@ -99,51 +99,6 @@ $('.valid-form-send').click(function () {
         }
     });
 });
-$.fn.select2.amd.define('select2/i18n/ru',[],function () {
-    // Russian
-    return {
-        errorLoading: function () {
-            return 'Результат не может быть загружен.';
-        },
-        inputTooLong: function (args) {
-            var overChars = args.input.length - args.maximum;
-            var message = 'Пожалуйста, удалите ' + overChars + ' символ';
-            if (overChars >= 2 && overChars <= 4) {
-                message += 'а';
-            } else if (overChars >= 5) {
-                message += 'ов';
-            }
-            return message;
-        },
-        inputTooShort: function (args) {
-            var remainingChars = args.minimum - args.input.length;
-
-            var message = 'Пожалуйста, введите ' + remainingChars + ' или более символов';
-
-            return message;
-        },
-        loadingMore: function () {
-            return 'Загружаем ещё ресурсы…';
-        },
-        maximumSelected: function (args) {
-            var message = 'Вы можете выбрать ' + args.maximum + ' элемент';
-
-            if (args.maximum  >= 2 && args.maximum <= 4) {
-                message += 'а';
-            } else if (args.maximum >= 5) {
-                message += 'ов';
-            }
-
-            return message;
-        },
-        noResults: function () {
-          return 'Ничего не найдено';
-        },
-        searching: function () {
-          return 'Поиск…';
-        }
-    };
-});
 jQuery(function ($) {
     $(document).ready(function () {
         $('.open-hidden').change(function(e){
@@ -167,11 +122,6 @@ jQuery(function ($) {
             else {
                 $(hiddenEl).fadeIn(300);
             }
-        });
-        
-        $('.customselect').select2({
-            placeholder: 'Выберите ответ',
-            language: "ru"
         });
     });
 });
